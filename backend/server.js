f80bddb82fbd0ee177412d62844b2a3b0f459d90
balanced0 <alvichowdhury013@gmail.com>
2026-08-24 17:55:39 +0600

    initialized backend server with Express and CORS

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`ReliefLink backend running on http://localhost:${port}`)
+})
